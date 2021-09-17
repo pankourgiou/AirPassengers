8d4c27740349dae4e5ba5c10ad9015808e6c4f9f
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-09-17 13:19:22 +0300

    Update Bruce.py

--- Bruce.py
+++ Bruce.py
@@ -3,76 +3,10 @@ import time
 
 t1 = time.time()
 
-x = "Let's get down, let's get down to business"
-x1 = "Give you one more night, one more night to get this"
-x2 = "We've had a million, million nights just like this"
-x3 = "So let's get down, let's get down to business"
-x4 = "Mama please don't worry 'bout me"
-x5 = "'Cause I'm about to let my heart speak"
-x6 = "My friends keep telling me to leave this"
-x7 = "So let's get down, let's get down to business"
-x8 = "Let's get down, let's get down to business"
-x9 = "Give you one more night, one more night to get this"
-x10 = "We've had a million, million nights just like this"
-x11 = "So let's get down, let's get down to business"
-x12 = "Let's get down, let's get down to business"
-x13 = "Give you one more night, one more night to get this"
-x14 = "We've had a million, million nights just like this"
-x15 = "So let's get down, let's get down to business"
-x16 = "Back and forth, back and forth with the bullshit"
-x17 = "I know I said it before, I don't mean it"
-x18 = "It's been a while since I had your attention"
-x19 = "So it might hurt to hear this"
-x20 = "Dreams we have don't ever fall away"
-x21 = "We can't leave 'em if we stay the same"
-x22 = "And I can't do this for another day"
-x23 = "So let's get down, let's get down to business"
-x24 = "Let's get down, let's get down to business"
-x25 = "Give you one more night, one more night to get this"
-x26 = "We've had a million, million nights just like this"
-x27 = "So let's get down, let's get down to business"
-x28 = "Let's get down, let's get down to business (let's get down)"
-x29 = "Give you one more night, one more night to get this (get this)"
-x30 = "We've had a million, million nights just like this"
-x31 = "So let's get down, let's get down to business"
-x32 = "Ooh, yeah, yeah"
+x = ("nature ignores sadness")
 
-print("White man came across the sea")
-print("He brought us pain and misery")
-print("He killed our tribes, he killed our creed")
-print("He took our game for his own need")
-print("Run to the hills")
-print("Run for your lives")
-print("Run to the hills")
-print("Run for your lives")
-print("White man came across the sea")
-print("He brought us pain and misery")
-print("He killed our tribes, he killed our creed")
-print("He took our game for his own need")
-print("Run to the hills")
-print("Run for your lives")
-print("Run to the hills")
-print("Run for your lives")
-print("White man came across the sea")
-print("He brought us pain and misery")
-print("He killed our tribes, he killed our creed")
-print("He took our game for his own need")
-print("Run to the hills")
-print("Run for your lives")
-print("Run to the hills")
-print("Run for your lives")
-print("White man came across the sea")
-print("He brought us pain and misery")
-print("He killed our tribes, he killed our creed")
-print("He took our game for his own need")
-print("Run to the hills")
-print("Run for your lives")
-print("Run to the hills")
-print("Run for your lives")
-
-
+print(bool(x))
 
 t2 = time.time()
 t = t2 - t1
 print("Elapsed time is : ", t, " seconds")
-
